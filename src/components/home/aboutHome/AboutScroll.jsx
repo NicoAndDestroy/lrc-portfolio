708fbe041abd6c5a123134e87b664b76fd5c0cb6
Nicolas Morales <jnicolasmoraless@hotmail.com>
2025-12-31 16:54:16 -0500

    Update AboutScroll.jsx

--- src/components/home/aboutHome/AboutScroll.jsx
+++ src/components/home/aboutHome/AboutScroll.jsx
@@ -9,17 +9,26 @@ export default function AboutScroll({ children }) {
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.from(".about-reveal", {
-        opacity: 0,
-        y: 40,
-        duration: 1,
-        ease: "power3.out",
-        stagger: 0.2,
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          start: "top 70%",
-        },
-      });
+      gsap.fromTo(
+  ".about-reveal",
+  {
+    opacity: 0,
+    y: 40,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    ease: "power2.out",
+    stagger: 0.2,
+    scrollTrigger: {
+      trigger: sectionRef.current,
+      start: "top 75%",
+      end: "top 45%",
+      scrub: true,
+    },
+  }
+);
+
     }, sectionRef);
 
     return () => ctx.revert();
